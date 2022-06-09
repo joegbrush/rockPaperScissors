53eaa69567cb62078b9ac0eff62b9b88925f66a1
Joe Brush <joegbrush@gmail.com>
2022-06-09 17:08:44 -0500

    computerPlay function created

--- rps.js
+++ rps.js
@@ -1,1 +1,10 @@
-let message = "Hello from consolezzz.";
+function computerPlay() {
+    let x = Math.random();
+    if(x < .33){
+        return "Rock";
+    } else if(x < .66){
+        return "Paper";
+    }else{
+        return "Scissors";
+    }
+}
