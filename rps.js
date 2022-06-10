6225ff8a2557e697f218aaf70daded12df1a76f4
Joe Brush <joegbrush@gmail.com>
2022-06-10 10:13:15 -0500

    gamePlay function added return to console one round of play

--- rps.js
+++ rps.js
@@ -7,4 +7,20 @@ function computerPlay() {
     }else{
         return "Scissors";
     }
-}
+};
+
+// Play a round
+
+let playerSelection;
+let computerSelection;
+
+function gamePlay(playerSelection, computerSelection) { 
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase(); 
+   
+    if(playerSelection == 'rock' && computerSelection == 'paper'){
+       return "You lose, paper beats rock.";
+    }
+};
+
+
